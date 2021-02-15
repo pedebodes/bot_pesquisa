2300be5e1f4300bd4314a9accb31b8bcae3a1fd4
Gilberto <gilbertoliveir@gmail.com>
2021-02-15 10:06:34 -0300

    incluindo .env e arquivo de criação das tabelas

---
+++ migrate.py
@@ -0,0 +1,46 @@
+
+import os
+from dotenv import load_dotenv, find_dotenv
+
+from sqlalchemy import Column, Integer, String, JSON
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+load_dotenv(find_dotenv())
+Base = declarative_base()
+
+
+engine = create_engine(os.environ.get("DATABASE_URL"), echo=bool(os.environ.get("ECHO")))
+class UrlBase(Base):
+    __tablename__='url_base'    
+    id = Column(Integer, primary_key=True)
+    dominio = Column(String)    
+    url =Column(String) 
+    cnpj = Column(String)
+    telefone_fixo = Column(String)
+    telefone_celular = Column(String)
+    cep = Column(String)
+    email = Column(String)
+    endereco = Column(JSON)
+    dados_cnpj = Column(JSON)
+    
+
+class UrlIgnorar(Base):
+    __tablename__='url_ignorar'
+    id = Column(Integer, primary_key=True)
+    dominio = Column(String)    
+
+
+def create_tables():
+    try:
+        Base.metadata.create_all(engine)
+        print ("Executado com sucesso.")
+    except:
+        print ("Ocorreu erro na criação das tabelas, favor verificar")
+
+
+def main():
+    create_tables()
+    
+
+if __name__=='__main__':
+    main()
