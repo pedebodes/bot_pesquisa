7d41f23624aae6dfa6eb2cf874823d109409163e
Gilberto <gilbertoliveir@gmail.com>
2021-02-26 10:58:59 -0300

    add

--- util.py
+++ util.py
@@ -1,11 +1,8 @@
 import requests
-from requests.exceptions import HTTPError
 from fake_headers import Headers
 from time import sleep
 import random
 import re
-import json
-from fake_useragent import UserAgent
 
 
 def removeDuplicado(valor):
@@ -20,32 +17,28 @@ def regex(opcao,arquivo):
         'telefoneAPI': re.compile(r'\=+[\d]{13}\&+')
         }
     
-    return json.dumps(
-        removeDuplicado(
-            re.findall(
-                tipoRegex.get(opcao), arquivo
-                )
-            )
-        )
+    return re.findall(tipoRegex.get(opcao), arquivo)
     
-
     
 def getRequest(url):
+    
     try:
         header = Headers(
             headers=False
         )
         sleep(random.randint(2,30)) 
-        return requests.get(url, headers=header.generate())
+        return requests.get(url, headers=header.generate(),timeout=5)
+        
+    except requests.exceptions.RequestException as err:
+        return err
+    except requests.exceptions.HTTPError as errh:
+        return errh
+    except requests.exceptions.ConnectionError as errc:
+        return errc
+    except requests.exceptions.Timeout as errt:
+        return errt
+    
 
+def parse_input(i):
+    return ''.join(x for x in i if x.isdigit())
 
-    except HTTPError as http_err:
-        print(f'Erro HTTP: {http_err}')
-        return False
-    except Exception as err:
-        print(f'Outro erro desconhecido: {err}')    
-        return False
-    except:
-        print('Erro indefinido')
-        return False
-       
