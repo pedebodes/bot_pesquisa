50cd4ee567732146f59f4b95a3c70e3df785fcee
Gilberto <gilbertoliveir@gmail.com>
2021-03-05 10:50:40 -0300

    inicio API

---
+++ api.py
@@ -0,0 +1,16 @@
+from resources import *
+from flask import Flask,  request
+from flask_restful import Resource, Api
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+
+        
+api.add_resource(DominiosIgnorados, '/dominios_ignorar/')
+api.add_resource(DominiosIgnoradosRemove,
+                 '/dominios_ignorar/<int:url_id>/')
+
+api.add_resource(PesquisaList, '/termos_pesquisados/')
+
