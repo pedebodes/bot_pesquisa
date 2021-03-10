5e8de9f9c4b6ac8289b028d6bf3ed06d8d730f9d
Gilberto <gilbertoliveir@gmail.com>
2021-03-10 12:00:06 -0300

    ajustes

--- api.py
+++ api.py
@@ -8,11 +8,9 @@ api = Api(app)
 
 
 api.add_resource(DominiosIgnorados, '/dominios_ignorar/')
-api.add_resource(DominiosIgnoradosRemove,
-                 '/dominios_ignorar/<int:url_id>/')
-
-api.add_resource(PesquisaList, '/termos_pesquisados/')
+api.add_resource(DominiosIgnoradosRemove, '/dominios_ignorar/<int:url_id>/')
 api.add_resource(Pesquisa, '/pesquisa/')
+api.add_resource(Resultado, '/resultado/<int:pesquisa_id>')
 api.add_resource(DadosUrl, '/coletar_dados_url/')
 api.add_resource(DadosTermo, '/coletar_dados_termo/')
 api.add_resource(ReprocessaPesquisaFalha,
